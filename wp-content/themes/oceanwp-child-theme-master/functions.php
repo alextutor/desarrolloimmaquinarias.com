6237e8db5fec527410cc8c8b8d3fdb8afc4649ab
DESKTOP-7PIR9R2\alex <soporte@unidadvirtual.com>
2021-10-22 19:01:40 -0500

    avances css solucionado problema no cargaba detalle de producto

--- wp-content/themes/oceanwp-child-theme-master/functions.php
+++ wp-content/themes/oceanwp-child-theme-master/functions.php
@@ -33,3 +33,4 @@ add_action( 'wp_enqueue_scripts', 'oceanwp_child_enqueue_parent_style' );
 add_filter( 'gutenberg_use_widgets_block_editor', '__return_false' );
 add_filter( 'use_widgets_block_editor', '__return_false' );
 
+
